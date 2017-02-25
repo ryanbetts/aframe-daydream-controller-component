ca4c6ab025e63944b29dcc077e0f07f4ab00ec0c
Ryan Betts <ryan@ishuman.co>
2017-02-24 16:03:57 -0800

    Updates

--- build.js
+++ build.js
@@ -1,268 +1,8 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-/*
- * From Boris Smus' (Google) Ray Input library:
- * https://github.com/borismus/ray-input/blob/master/src/orientation-arm-model.js
- */
-
-/*
- * Copyright 2016 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-var HEAD_ELBOW_OFFSET = new THREE.Vector3(0.155, -0.465, -0.15);
-var ELBOW_WRIST_OFFSET = new THREE.Vector3(0, 0, -0.25);
-var WRIST_CONTROLLER_OFFSET = new THREE.Vector3(0, 0, 0.05);
-var ARM_EXTENSION_OFFSET = new THREE.Vector3(-0.08, 0.14, 0.08);
-
-var ELBOW_BEND_RATIO = 0.4; // 40% elbow, 60% wrist.
-var EXTENSION_RATIO_WEIGHT = 0.4;
-
-var MIN_ANGULAR_SPEED = 0.61; // 35 degrees per second (in radians).
-
-/**
- * Represents the arm model for the Daydream controller. Feed it a camera and
- * the controller. Update it on a RAF.
- *
- * Get the model's pose using getPose().
- */
-
-var OrientationArmModel = function () {
-  function OrientationArmModel() {
-    _classCallCheck(this, OrientationArmModel);
-
-    this.isLeftHanded = false;
-
-    // Current and previous controller orientations.
-    this.controllerQ = new THREE.Quaternion();
-    this.lastControllerQ = new THREE.Quaternion();
-
-    // Current and previous head orientations.
-    this.headQ = new THREE.Quaternion();
-
-    // Current head position.
-    this.headPos = new THREE.Vector3();
-
-    // Positions of other joints (mostly for debugging).
-    this.elbowPos = new THREE.Vector3();
-    this.wristPos = new THREE.Vector3();
-
-    // Current and previous times the model was updated.
-    this.time = null;
-    this.lastTime = null;
-
-    // Root rotation.
-    this.rootQ = new THREE.Quaternion();
-
-    // Current pose that this arm model calculates.
-    this.pose = {
-      orientation: new THREE.Quaternion(),
-      position: new THREE.Vector3()
-    };
-  }
-
-  /**
-   * Methods to set controller and head pose (in world coordinates).
-   */
-
-
-  _createClass(OrientationArmModel, [{
-    key: 'setControllerOrientation',
-    value: function setControllerOrientation(quaternion) {
-      this.lastControllerQ.copy(this.controllerQ);
-      this.controllerQ.copy(quaternion);
-    }
-  }, {
-    key: 'setHeadOrientation',
-    value: function setHeadOrientation(quaternion) {
-      this.headQ.copy(quaternion);
-    }
-  }, {
-    key: 'setHeadPosition',
-    value: function setHeadPosition(position) {
-      this.headPos.copy(position);
-    }
-  }, {
-    key: 'setLeftHanded',
-    value: function setLeftHanded(isLeftHanded) {
-      // TODO(smus): Implement me!
-      this.isLeftHanded = isLeftHanded;
-    }
-
-    /**
-     * Called on a RAF.
-     */
-
-  }, {
-    key: 'update',
-    value: function update() {
-      this.time = performance.now();
-
-      // If the controller's angular velocity is above a certain amount, we can
-      // assume torso rotation and move the elbow joint relative to the
-      // camera orientation.
-      var headYawQ = this.getHeadYawOrientation_();
-      var timeDelta = (this.time - this.lastTime) / 1000;
-      var angleDelta = this.quatAngle_(this.lastControllerQ, this.controllerQ);
-      var controllerAngularSpeed = angleDelta / timeDelta;
-      if (controllerAngularSpeed > MIN_ANGULAR_SPEED) {
-        // Attenuate the Root rotation slightly.
-        this.rootQ.slerp(headYawQ, angleDelta / 10);
-      } else {
-        this.rootQ.copy(headYawQ);
-      }
-
-      // We want to move the elbow up and to the center as the user points the
-      // controller upwards, so that they can easily see the controller and its
-      // tool tips.
-      var controllerEuler = new THREE.Euler().setFromQuaternion(this.controllerQ, 'YXZ');
-      var controllerXDeg = THREE.Math.radToDeg(controllerEuler.x);
-      var extensionRatio = this.clamp_((controllerXDeg - 11) / (50 - 11), 0, 1);
-
-      // Controller orientation in camera space.
-      var controllerCameraQ = this.rootQ.clone().inverse();
-      controllerCameraQ.multiply(this.controllerQ);
-
-      // Calculate elbow position.
-      var elbowPos = this.elbowPos;
-      elbowPos.copy(this.headPos).add(HEAD_ELBOW_OFFSET);
-      var elbowOffset = new THREE.Vector3().copy(ARM_EXTENSION_OFFSET);
-      elbowOffset.multiplyScalar(extensionRatio);
-      elbowPos.add(elbowOffset);
-
-      // Calculate joint angles. Generally 40% of rotation applied to elbow, 60%
-      // to wrist, but if controller is raised higher, more rotation comes from
-      // the wrist.
-      var totalAngle = this.quatAngle_(controllerCameraQ, new THREE.Quaternion());
-      var totalAngleDeg = THREE.Math.radToDeg(totalAngle);
-      var lerpSuppression = 1 - Math.pow(totalAngleDeg / 180, 4); // TODO(smus): ???
-
-      var elbowRatio = ELBOW_BEND_RATIO;
-      var wristRatio = 1 - ELBOW_BEND_RATIO;
-      var lerpValue = lerpSuppression * (elbowRatio + wristRatio * extensionRatio * EXTENSION_RATIO_WEIGHT);
-
-      var wristQ = new THREE.Quaternion().slerp(controllerCameraQ, lerpValue);
-      var invWristQ = wristQ.inverse();
-      var elbowQ = controllerCameraQ.clone().multiply(invWristQ);
-
-      // Calculate our final controller position based on all our joint rotations
-      // and lengths.
-      /*
-      position_ =
-        root_rot_ * (
-          controller_root_offset_ +
-      2:      (arm_extension_ * amt_extension) +
-      1:      elbow_rot * (kControllerForearm + (wrist_rot * kControllerPosition))
-        );
-      */
-      var wristPos = this.wristPos;
-      wristPos.copy(WRIST_CONTROLLER_OFFSET);
-      wristPos.applyQuaternion(wristQ);
-      wristPos.add(ELBOW_WRIST_OFFSET);
-      wristPos.applyQuaternion(elbowQ);
-      wristPos.add(this.elbowPos);
-
-      var offset = new THREE.Vector3().copy(ARM_EXTENSION_OFFSET);
-      offset.multiplyScalar(extensionRatio);
-
-      var position = new THREE.Vector3().copy(this.wristPos);
-      position.add(offset);
-      position.applyQuaternion(this.rootQ);
-
-      var orientation = new THREE.Quaternion().copy(this.controllerQ);
-
-      // Set the resulting pose orientation and position.
-      this.pose.orientation.copy(orientation);
-      this.pose.position.copy(position);
-
-      this.lastTime = this.time;
-    }
-
-    /**
-     * Returns the pose calculated by the model.
-     */
-
-  }, {
-    key: 'getPose',
-    value: function getPose() {
-      return this.pose;
-    }
-
-    /**
-     * Debug methods for rendering the arm model.
-     */
-
-  }, {
-    key: 'getForearmLength',
-    value: function getForearmLength() {
-      return ELBOW_WRIST_OFFSET.length();
-    }
-  }, {
-    key: 'getElbowPosition',
-    value: function getElbowPosition() {
-      var out = this.elbowPos.clone();
-      return out.applyQuaternion(this.rootQ);
-    }
-  }, {
-    key: 'getWristPosition',
-    value: function getWristPosition() {
-      var out = this.wristPos.clone();
-      return out.applyQuaternion(this.rootQ);
-    }
-  }, {
-    key: 'getHeadYawOrientation_',
-    value: function getHeadYawOrientation_() {
-      var headEuler = new THREE.Euler().setFromQuaternion(this.headQ, 'YXZ');
-      headEuler.x = 0;
-      headEuler.z = 0;
-      var destinationQ = new THREE.Quaternion().setFromEuler(headEuler);
-      return destinationQ;
-    }
-  }, {
-    key: 'clamp_',
-    value: function clamp_(value, min, max) {
-      return Math.min(Math.max(value, min), max);
-    }
-  }, {
-    key: 'quatAngle_',
-    value: function quatAngle_(q1, q2) {
-      var vec1 = new THREE.Vector3(0, 0, -1);
-      var vec2 = new THREE.Vector3(0, 0, -1);
-      vec1.applyQuaternion(q1);
-      vec2.applyQuaternion(q2);
-      return vec1.angleTo(vec2);
-    }
-  }]);
-
-  return OrientationArmModel;
-}();
-
-exports.default = OrientationArmModel;
-
-},{}],2:[function(require,module,exports){
 require('aframe');
 require('../index.js');
 
-},{"../index.js":3,"aframe":4}],3:[function(require,module,exports){
+},{"../index.js":2,"aframe":3}],2:[function(require,module,exports){
 /* global AFRAME */
 
 if (typeof AFRAME === 'undefined') {
@@ -278,14 +18,10 @@ var DAYDREAM_CONTROLLER_MODEL_OBJ_MTL = 'https://raw.githubusercontent.com/Techn
 
 var GAMEPAD_ID_PREFIX = 'Daydream Controller';
 
-/* grab smus' orientation arm model constructor */
-var OrientationArmModel = require('./OrientationArmModel').default;
-
 /**
  * Daydream Controller component for A-Frame.
  */
 AFRAME.registerComponent('daydream-controller', {
-
   /**
    * Set if component needs multiple instancing.
    */
@@ -296,7 +32,9 @@ AFRAME.registerComponent('daydream-controller', {
     buttonTouchedColor: {default: 'yellow'},  // Light blue.
     buttonPressedColor: {default: 'orange'},  // Light blue.
     model: {default: true},
-    rotationOffset: {default: 0} // use -999 as sentinel value to auto-determine based on hand
+    rotationOffset: {default: 0}, // use -999 as sentinel value to auto-determine based on hand
+    eyesToElbow: {default: {x: 0.175, y: -0.3, z: -0.03}}, // vector from eyes to elbow (divided by user height)
+    forearm: {default: {x: 0, y: 0, z: -0.175}}, // vector from eyes to elbow (divided by user height)
   },
 
   // buttonId
@@ -317,7 +55,6 @@ AFRAME.registerComponent('daydream-controller', {
     this.isControllerPresent = trackedControlsUtils.isControllerPresent; // to allow mock
     this.buttonStates = {};
     this.previousAxis = [];
-    this.armModel = new OrientationArmModel();
     this.onModelLoaded = bind(this.onModelLoaded, this);
     this.checkIfControllerPresent = bind(this.checkIfControllerPresent, this);
     this.onGamepadConnected = bind(this.onGamepadConnected, this);
@@ -372,7 +109,7 @@ AFRAME.registerComponent('daydream-controller', {
       });
     } else {
       this.controller = null;
-      this.el.removeAttribue('obj-model');
+      this.el.removeAttribute('obj-model');
       this.el.removeEventListener('model-loaded', this.onModelLoaded);
     }
   },
@@ -394,7 +131,7 @@ AFRAME.registerComponent('daydream-controller', {
     buttonMeshes.system = controllerObject3D.getObjectByName('HomeButton_HomeButton_Cylinder.005');
     buttonMeshes.trackpad = controllerObject3D.getObjectByName('TouchPad_TouchPad_Cylinder.003');
     // Offset pivot point
-    controllerObject3D.position.set(0, -0.015, 0.04);
+    controllerObject3D.position.set(0, 0, -0.04);
   },
 
   updateButtonModel: function (buttonName, state) {
@@ -410,40 +147,51 @@ AFRAME.registerComponent('daydream-controller', {
   },
 
   updatePose: (function () {
-    var controllerEuler = new THREE.Euler();
+    var offset = new THREE.Vector3();
+    var position = new THREE.Vector3();
     var controllerQuaternion = new THREE.Quaternion();
+    var controllerEuler = new THREE.Euler(0, 0, 0, 'YXZ');
     return function () {
       var controller = this.controller;
       var pose = controller.pose;
       var el = this.el;
       var camera = this.el.sceneEl.camera;
-      var orientation;
-      var armModelPose;
-      if (!controller) { return; }
-      // Feed camera and controller into the arm model.
-      camera = this.el.sceneEl.camera;
-      this.armModel.setHeadOrientation(camera.quaternion);
-      this.armModel.setHeadPosition(camera.position);
-      // feed the controller orientation into the arm model.
-      orientation = pose.orientation || [0, 0, 0, 1];
-      controllerQuaternion.fromArray(orientation);
-      this.armModel.setControllerOrientation(controllerQuaternion);
-      // Get resulting pose
-      this.armModel.update();
-      armModelPose = this.armModel.getPose();
-      controllerEuler.setFromQuaternion(armModelPose.orientation);
-      // update the rotation
-      el.setAttribute('rotation', {
-        x: THREE.Math.radToDeg(controllerEuler.x),
-        y: THREE.Math.radToDeg(controllerEuler.y),
-        z: THREE.Math.radToDeg(controllerEuler.z) + this.data.rotationOffset
-      });
-      // update the position
-      el.setAttribute('position', {
-        x: armModelPose.position.x,
-        y: armModelPose.position.y,
-        z: armModelPose.position.z
-      });
+      var cameraComponent = camera.el.components.camera;
+      var eyesToElbow = this.data.eyesToElbow;
+      var forearm = this.data.forearm;
+
+      // get camera position
+      position.copy(camera.el.object3D.position);
+
+      // set offset for degenerate "arm model" to elbow
+      offset.set(
+	this.data.hand === 'left' ? -eyesToElbow.x : eyesToElbow.x, // hand is to your left, or right
+        eyesToElbow.y, // lower than your eyes
+        eyesToElbow.z); // slightly out in front
+      // scale offset by user height
+      offset.multiplyScalar(cameraComponent.data.userHeight);
+      // apply camera Y rotation (not X or Z, so you can look down at your hand)
+      offset.applyAxisAngle(camera.el.object3D.up, camera.el.object3D.rotation.y);
+      // apply rotated offset to camera position
+      position.add(offset);
+
+      // set offset for degenerate "arm model" forearm
+      offset.set(forearm.x, forearm.y, forearm.z); // forearm sticking out from elbow
+      // scale offset by user height
+      offset.multiplyScalar(cameraComponent.data.userHeight);
+      // apply controller X and Y rotation (tilting up/down/left/right is usually moving the arm)
+      controllerQuaternion.fromArray(pose.orientation || [0, 0, 0, 1]);
+      controllerEuler.setFromQuaternion(controllerQuaternion);
+      controllerEuler.set(controllerEuler.x, controllerEuler.y, 0);
+      offset.applyEuler(controllerEuler);
+      // apply rotated offset to camera position
+      position.add(offset);
+
+      // set as controller position
+      el.setAttribute('position', { x: position.x, y: position.y, z: position.z });
+
+      // set controller rotation directly from pose, if any (NO EULER!)
+      el.object3D.quaternion.copy(controllerQuaternion);
     };
   })(),
 
@@ -532,18 +280,21 @@ AFRAME.registerComponent('daydream-controller', {
       evtName = 'end';
     }
     previousButtonState.touched = buttonState.touched;
-    this.el.emit('touch' + evtName, {
-      id: id,
-      state: previousButtonState,
-      axis: this.controller.axes
-    });
+    this.el.dispatchEvent(new CustomEvent('touch' + evtName, {
+      'touches': [], // avoid exception in TouchPanner due to namespace collision
+      'detail': {
+        id: id,
+        state: previousButtonState,
+        axis: this.controller.axes
+      }
+    }));
     buttonName = this.mapping['button' + id];
     this.updateButtonModel(buttonName, 'touch' + evtName);
     return true;
   }
 });
 
-},{"./OrientationArmModel":1}],4:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 (function (global){
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.AFRAME = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 'use strict';
@@ -70074,4 +69825,4 @@ module.exports = getWakeLock();
 
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}]},{},[2]);
+},{}]},{},[1]);
